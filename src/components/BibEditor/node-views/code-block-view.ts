39d0b452f99b9b2c80950ba4e6f6a33ddc4bfdb4
ShenQingchuan <tmyeve@163.com>
2021-03-17 22:19:02 +0800

    fix: upgrade dep.

--- src/components/BibEditor/node-views/code-block-view.ts
+++ src/components/BibEditor/node-views/code-block-view.ts
@@ -1,16 +1,16 @@
-import { Node } from "prosemirror-model";
-import { EditorView } from "prosemirror-view";
-import { exitCode } from "prosemirror-commands";
-import { undo, redo } from "prosemirror-history";
-import * as CodeMirror from "codemirror";
+import { Node } from 'prosemirror-model';
+import { EditorView } from 'prosemirror-view';
+import { exitCode } from 'prosemirror-commands';
+import { undo, redo } from 'y-prosemirror';
+import * as CodeMirror from 'codemirror';
 import {
   EditorState,
   Selection,
   TextSelection,
-  Transaction,
-} from "prosemirror-state";
-import { EditorSchema } from "../editor-schema";
-import { keymap } from "prosemirror-keymap";
+  Transaction
+} from 'prosemirror-state';
+import { EditorSchema } from '../editor-schema';
+import { keymap } from 'prosemirror-keymap';
 
 function computeChange(oldVal: any, newVal: any) {
   if (oldVal == newVal) return null;
@@ -30,7 +30,7 @@ function computeChange(oldVal: any, newVal: any) {
   return { from: start, to: oldEnd, text: newVal.slice(start, newEnd) };
 }
 function arrowHandler(
-  dir: "up" | "down" | "left" | "right" | "forward" | "backward"
+  dir: 'up' | 'down' | 'left' | 'right' | 'forward' | 'backward'
 ) {
   return (
     state: EditorState,
@@ -38,13 +38,13 @@ function arrowHandler(
     view?: EditorView
   ) => {
     if (state.selection.empty && view?.endOfTextblock(dir)) {
-      let side = dir == "left" || dir == "up" ? -1 : 1,
+      let side = dir == 'left' || dir == 'up' ? -1 : 1,
         $head = state.selection.$head;
       let nextPos = Selection.near(
         state.doc.resolve(side > 0 ? $head.after() : $head.before()),
         side
       );
-      if (nextPos.$head && nextPos.$head.parent.type.name == "code_block") {
+      if (nextPos.$head && nextPos.$head.parent.type.name == 'code_block') {
         dispatch!(state.tr.setSelection(nextPos));
         return true;
       }
@@ -54,36 +54,36 @@ function arrowHandler(
 }
 
 const MIMEMap: Record<string, string> = {
-  c: "text/x-csrc",
-  cpp: "text/x-c++src",
-  "c++": "text/x-c++src",
-  csharp: "text/x-csharp",
-  "c#": "text/x-csharp",
-  java: "text/x-java",
-  scala: "text/x-scala",
-  javascript: "javascript",
-  js: "javascript",
-  objc: "text/x-objectivec",
-  objectivec: "text/x-objectivec",
-  php: "application/x-httpd-php",
-  py: "text/x-python",
-  python: "text/x-python",
-  go: "text/x-go",
-  swift: "text/x-swift",
-  lua: "text/x-lua",
-  sass: "text/x-sass",
-  rust: "text/x-rustsrc",
-  jsx: "text/jsx",
-  tsx: "text/typescript-jsx",
-  dart: "dart",
+  c: 'text/x-csrc',
+  cpp: 'text/x-c++src',
+  'c++': 'text/x-c++src',
+  csharp: 'text/x-csharp',
+  'c#': 'text/x-csharp',
+  java: 'text/x-java',
+  scala: 'text/x-scala',
+  javascript: 'javascript',
+  js: 'javascript',
+  objc: 'text/x-objectivec',
+  objectivec: 'text/x-objectivec',
+  php: 'application/x-httpd-php',
+  py: 'text/x-python',
+  python: 'text/x-python',
+  go: 'text/x-go',
+  swift: 'text/x-swift',
+  lua: 'text/x-lua',
+  sass: 'text/x-sass',
+  rust: 'text/x-rustsrc',
+  jsx: 'text/jsx',
+  tsx: 'text/typescript-jsx',
+  dart: 'dart'
 };
-const MIMEReflect = (lang?: string) => (lang ? MIMEMap[lang] || "" : "");
+const MIMEReflect = (lang?: string) => (lang ? MIMEMap[lang] || '' : '');
 
 export const arrowHandlersInCodeBlock = keymap({
-  ArrowLeft: arrowHandler("left"),
-  ArrowRight: arrowHandler("right"),
-  ArrowUp: arrowHandler("up"),
-  ArrowDown: arrowHandler("down"),
+  ArrowLeft: arrowHandler('left'),
+  ArrowRight: arrowHandler('right'),
+  ArrowUp: arrowHandler('up'),
+  ArrowDown: arrowHandler('down')
 });
 
 export default class CodeBlockView {
@@ -109,8 +109,8 @@ export default class CodeBlockView {
       lineNumbers: true,
       extraKeys: this.codeMirrorKeymap(),
       mode: MIMEReflect(this.lang),
-      scrollbarStyle: "null",
-      viewportMargin: Infinity,
+      scrollbarStyle: 'null',
+      viewportMargin: Infinity
     });
 
     // 代码编辑器的最外层节点是就是我们代码块的的 DOM 节点
@@ -120,19 +120,19 @@ export default class CodeBlockView {
     // 这个标记用来避免在外部编辑器和内部编辑器之间的循环更新
     this.updating = false;
     // 追踪是否改变已经发生，但是还没有传递出去
-    this.cm.on("beforeChange", () => (this.incomingChanges = true));
+    this.cm.on('beforeChange', () => (this.incomingChanges = true));
     // 将代码编辑器的更新传递给外层的 ProseMirror
-    this.cm.on("cursorActivity", () => {
+    this.cm.on('cursorActivity', () => {
       if (!this.updating && !this.incomingChanges) this.forwardSelection();
     });
-    this.cm.on("changes", () => {
+    this.cm.on('changes', () => {
       if (!this.updating) {
         this.valueChanged();
         this.forwardSelection();
       }
       this.incomingChanges = false;
     });
-    this.cm.on("focus", () => this.forwardSelection());
+    this.cm.on('focus', () => this.forwardSelection());
   }
 
   forwardSelection() {
@@ -144,8 +144,8 @@ export default class CodeBlockView {
   }
   asProseMirrorSelection(doc: Node) {
     let offset = this.getPos() + 1;
-    let anchor = this.cm.indexFromPos(this.cm.getCursor("anchor")) + offset;
-    let head = this.cm.indexFromPos(this.cm.getCursor("head")) + offset;
+    let anchor = this.cm.indexFromPos(this.cm.getCursor('anchor')) + offset;
+    let head = this.cm.indexFromPos(this.cm.getCursor('head')) + offset;
     return TextSelection.create(doc, anchor, head);
   }
   setSelection(anchor: number, head: number) {
@@ -171,18 +171,18 @@ export default class CodeBlockView {
   }
   codeMirrorKeymap() {
     let view = this.view;
-    let mod = /Mac/.test(navigator.platform) ? "Cmd" : "Ctrl";
+    let mod = /Mac/.test(navigator.platform) ? 'Cmd' : 'Ctrl';
     return CodeMirror.normalizeKeyMap({
-      Up: () => this.maybeEscape("line", -1),
-      Left: () => this.maybeEscape("char", -1),
-      Down: () => this.maybeEscape("line", 1),
-      Right: () => this.maybeEscape("char", 1),
+      Up: () => this.maybeEscape('line', -1),
+      Left: () => this.maybeEscape('char', -1),
+      Down: () => this.maybeEscape('line', 1),
+      Right: () => this.maybeEscape('char', 1),
       [`${mod}-Enter`]: () => {
         if (exitCode(view.state, view.dispatch)) view.focus();
       },
-      [`${mod}-Z`]: () => undo(view.state, view.dispatch),
-      [`Shift-${mod}-Z`]: () => redo(view.state, view.dispatch),
-      [`${mod}-Y`]: () => redo(view.state, view.dispatch),
+      [`${mod}-Z`]: () => undo(view.state),
+      [`Shift-${mod}-Z`]: () => redo(view.state),
+      [`${mod}-Y`]: () => redo(view.state)
     });
   }
   maybeEscape(unit: string, dir: number) {
@@ -190,7 +190,7 @@ export default class CodeBlockView {
     if (
       this.cm.somethingSelected() ||
       pos.line != (dir < 0 ? this.cm.firstLine() : this.cm.lastLine()) ||
-      (unit === "char" &&
+      (unit === 'char' &&
         pos.ch != (dir < 0 ? 0 : this.cm.getLine(pos.line).length))
     )
       return CodeMirror.Pass;
